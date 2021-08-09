ce08bfe8e268c969cdf1852dd365a21ef672d4a8
Serginiho <serginiho2985@gmail.com>
2021-08-09 17:51:15 +0300

    Update Main.java

--- src/com/organization/annotationprocessor/Main.java
+++ src/com/organization/annotationprocessor/Main.java
@@ -2,7 +2,7 @@ package com.organization.annotationprocessor;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is custom annotation processor, which extract interface");
+        System.out.println("This is custom annotation processor, which extract interface and create sql command");
         System.exit(0);
     }
 }
